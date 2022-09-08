1b8aa1ed2aebff0a650b2f5c148b202526819ace
Leestan360 <stanleymayore2@gmail.com>
2022-09-09 01:30:52 +0300

    Handle input two state to allow only numbers

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,53 +1,62 @@
 import React, { useState } from "react";
 
 function Input() {
-
-  const [inputOne, setInputOne] = useState("")
-  const [inputTwo, setInputTwo] = useState("")
-  const [inputThree, setInputThree] = useState("")
+  const [inputOne, setInputOne] = useState("");
+  const [inputTwo, setInputTwo] = useState("");
+  const [inputThree, setInputThree] = useState("");
 
   function handleInputOneChange(e) {
-    setInputOne(e.target.value)
+    setInputOne(e.target.value);
   }
 
-  function handleInputTwoChange(e) {
-    setInputTwo(e.target.value)
+  function handleInputTwoChange(e) {    
+    const re = /^[0-9\b]+$/;
+
+    // if value is not blank, then test the regex
+    if (e.target.value === '' || re.test(e.target.value)) {
+       setInputTwo(e.target.value)
+    }
   }
 
   function handleInputThreeChange(e) {
-    setInputThree(e.target.value)
+    setInputThree(e.target.value);
   }
+
+  // function alphaOnly(event) {
+  //   var key = event.keyCode;
+  //   return ((key >= 65 && key <= 90) || key === 8);
+  // };
 
   return (
     <div>
       <div>
-        <input 
-        onkeydown="return /[a-z]/i.test(event.key)"
-        className="border-2"
-        value={inputOne}
-        onChange={handleInputOneChange}
+        <input
+          type="text"
+          className="border-2"
+          value={inputOne}
+          onChange={handleInputOneChange}
         />
       </div>
 
       <div>
-        <input 
-        className="border-2"
-        value={inputTwo}
-        onChange={handleInputTwoChange}
+        <input
+          type="text"
+          className="border-2"
+          value={inputTwo}
+          onChange={handleInputTwoChange}
         />
       </div>
 
       <div>
-        <input 
-        className="border-2"
-        onkeydown="return /[a-z]/i.test(event.key)"
-        value={inputThree}
-        onChange={handleInputThreeChange}
+        <input
+          type="text"
+          className="border-2"
+          value={inputThree}
+          onChange={handleInputThreeChange}
         />
       </div>
-
     </div>
-  )
+  );
 }
 
 export default Input;
