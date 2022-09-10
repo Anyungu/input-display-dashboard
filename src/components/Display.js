59c50143ee5d9463f88dc9a62e62443a788b0c59
Leestan360 <stanleymayore2@gmail.com>
2022-09-10 15:15:59 +0300

    Reverse the inputs

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,18 +2,11 @@ import React from "react";
 
 function Display({inputOne, inputTwo, inputThree}) {
 
-  function reverseInputOne(inputOne){  
+  // Reverse the inputs
+  function reverseInput(str){  
     let reversed = "";      
-     for (let i = inputOne.length - 1; i >= 0; i--){         
-       reversed += inputOne[i];  
-     }     
-    return reversed;
-  }
-
-  function reverseInputTwo(inputOne){  
-    let reversed = "";      
-     for (let i = inputOne.length - 1; i >= 0; i--){         
-       reversed += inputOne[i];  
+     for (let i = str.length - 1; i >= 0; i--){         
+       reversed += str[i];  
      }     
     return reversed;
   }
@@ -21,9 +14,9 @@ function Display({inputOne, inputTwo, inputThree}) {
   // Display the inputs as they change
   return (
     <div className="mr-10">
-      <div className="border-2 border-black rounded-lg m-4 h-20 w-44">{inputThree}</div>
-      <div className="border-2 border-black rounded-lg m-4 h-20 w-44">{reverseInputTwo(inputTwo)}</div>
-      <div className="border-2 border-black rounded-lg m-4 h-20 w-44">{reverseInputOne(inputOne)}</div>
+      <div className="border-2 border-black rounded-lg m-4 h-20 w-44">{reverseInput(inputThree)}</div>
+      <div className="border-2 border-black rounded-lg m-4 h-20 w-44">{reverseInput(inputTwo)}</div>
+      <div className="border-2 border-black rounded-lg m-4 h-20 w-44">{reverseInput(inputOne)}</div>
     </div>
   );
 }
