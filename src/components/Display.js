dbd08b57586f00550f62b7d38178982036487a80
Leestan360 <stanleymayore2@gmail.com>
2022-09-10 15:05:52 +0300

    Reverse input two

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,15 @@ import React from "react";
 
 function Display({inputOne, inputTwo, inputThree}) {
 
-  function reverseInput(inputOne){  
+  function reverseInputOne(inputOne){  
+    let reversed = "";      
+     for (let i = inputOne.length - 1; i >= 0; i--){         
+       reversed += inputOne[i];  
+     }     
+    return reversed;
+  }
+
+  function reverseInputTwo(inputOne){  
     let reversed = "";      
      for (let i = inputOne.length - 1; i >= 0; i--){         
        reversed += inputOne[i];  
@@ -14,8 +22,8 @@ function Display({inputOne, inputTwo, inputThree}) {
   return (
     <div className="mr-10">
       <div className="border-2 border-black rounded-lg m-4 h-20 w-44">{inputThree}</div>
-      <div className="border-2 border-black rounded-lg m-4 h-20 w-44">{inputTwo}</div>
-      <div className="border-2 border-black rounded-lg m-4 h-20 w-44">{reverseInput(inputOne)}</div>
+      <div className="border-2 border-black rounded-lg m-4 h-20 w-44">{reverseInputTwo(inputTwo)}</div>
+      <div className="border-2 border-black rounded-lg m-4 h-20 w-44">{reverseInputOne(inputOne)}</div>
     </div>
   );
 }
